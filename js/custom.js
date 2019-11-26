a4eab57190836224cdec64b8fe975d2d31602c23
Kumail Ali <kumail-ali@users.noreply.github.com>
2019-11-26 12:53:51 +0500

    page 2 done

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,28 @@
 
-
-$(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    center:true,
+    autoplay:false,
+    smartSpeed: 1000,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
         }
-    })
-    $('.owl-next').html('<i class="fas fa-chevron-right"></i>');
-    $('.owl-prev').html('<i class="fas fa-chevron-left"></i>');
-});
+    }
+})
+$('.owl-next').html('<i class="fas fa-chevron-right"></i>');
+$('.owl-prev').html('<i class="fas fa-chevron-left"></i>');
 
 
 
