47ff71e5d7f68b5d4a54229091268cb45787891b
Kumail Ali <kumail-ali@users.noreply.github.com>
2019-12-02 15:53:45 +0500

    map and slider

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,28 @@
 
+$('#owl-demo2').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav:true,
+    autoplayTimeout:5000,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+
+
+        },
+        600: {
+            items: 1,
+
+        },
+        1000: {
+            items: 1
+
+        }
+    }
+})
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
